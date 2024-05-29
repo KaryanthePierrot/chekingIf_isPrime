b839fa423ce1c6cf1927d8d3126fa307f7a47c06
Wes (Karyan) <113315411+KaryanthePierrot@users.noreply.github.com>
2024-05-29 13:38:44 -0300

    Add files via upload

---
+++ aticidade 14 -- Verificação de Primos/atividade 14/atividade 14 -- verificação de primos.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int n, d;
+    puts("O seu numero e primo ou nao? Vamos descobrir, digite seu numero: ");
+    scanf("%d", &n);
+    
+    for (d = 2; d <= ceil(sqrt(n)); d++) {
+        if (n % d == 0) {
+            printf("%d nao e primo!\n", n);
+            return 0;
+        }
+    }
+
+    printf("%d e primo!\n", n);
+    return 0;
+}
